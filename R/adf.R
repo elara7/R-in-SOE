1c84fb64217e8fd3571c5fcc954afaee8595d748
elara7 <elara15598@gmail.com>
2016-05-17 01:29:18 +0800

    bigdata class finish

---
+++ R/adf.R
@@ -0,0 +1,5 @@
+Three models for ADF test       
+model 1:$\Delta X_t = \delta X_{t-1}+\sum _{i=1}^{m}\beta_{i}\DeltaX_{t-i}+\epsilon_t$          
+        model 2:$\Delta X_t = \alpha+\delta X_{t-1}+\sum _{i=1}^{m}\beta_{i}\DeltaX_{t-i}+\epsilon_t$          
+        model 3:$\Delta X_t = \alpha+\beta t+\delta X_{t-1}+\sum _{i=1}^{m}\beta_{i}\DeltaX_{t-i}+\epsilon_t$          
+        $H_0:\delta = 0$,that means there exist a unit root or nonstationary.Stare from model 3,until model 1.Once one model reject $H_0$,it can be considered as a stationary series.
